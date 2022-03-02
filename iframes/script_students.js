ec88203bb76efd919f3529cb50e45f0eae66e869
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2022-03-01 21:16:13 -0500

    ->Ya se tienen las basese del proyecto solo falta añadir un poco mas de informacion y modificar el diseño

--- iframes/script_students.js
+++ iframes/script_students.js
@@ -2,6 +2,7 @@ var estudiantes= [
 	{
 		"Usuario": "Jorge_Arguello",
 		"ColorC": "#87CEEB",
+		"ColorB": "Black",
 		"Foto": "../img/JorgeArguello_Foto.jpg",
 
 		"Nombres": "ARGUELLO GUSQUI JORGE ALEXANDER",
@@ -15,21 +16,346 @@ var estudiantes= [
 		"Comida": "Pizza",
 		"Metas": "Estudiar ingeniería en informática, intentar ser uno de los mejores para vivir de lo que me gusta hacer que es la informática",
 	},
+	{
+		"Usuario": "Joyce_Canelos",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "CANELOS ARANDA JOYCE GARDENIA",
+		"FechaNacimiento": "15-10-2004",
+		"Signo": "Libra",
+		"Edad": "17",
+		"Sexo": "Femenino",
+		"Artista": "Maria Becerra",
+		"Color": "Negro",
+		"Hobbie": "Escuchar música ",
+		"Comida": "Camarón apanado ",
+		"Metas": "Entrar a la selección de ecuador, graduarme del colegio y ser una gran veterinaria",
+	},
+	{
+		"Usuario": "Ramiro_Chavez",
+		"ColorC": "#3863ff",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "CHAVEZ MEJIA RAMIRO ISRAEL",
+		"FechaNacimiento": " 04-10-2005",
+		"Signo": "Libra",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Bad Bunny",
+		"Color": "Azul",
+		"Hobbie": "Entrenar Basket",
+		"Comida": "Camarones apanados",
+		"Metas": "Jugar en la NBA",
+	},
+	{
+		"Usuario": "Nikol_Conde",
+		"ColorC": "#7b68fc",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "CONDE CHANGO ZHARICK NIKOL",
+		"FechaNacimiento": "23-12-2005",
+		"Signo": "Capricornio",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "Dua Lipa",
+		"Color": "Morado",
+		"Hobbie": "Ciclismo",
+		"Comida": "Camarones Apanados",
+		"Metas": "Graduarme y ser una doctora psicacultural para acompañarle Ami hermano en los tours y conocer todos los lugares de este mundo antes de morir",
+	},
+	{
+		"Usuario": "Erick_Estrella",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "ESTRELLA GARCIA ERICK JAIR",
+		"FechaNacimiento": "14-01-2006",
+		"Signo": "Capricornio",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Paulo Londra y Sebastian Yatra",
+		"Color": "Negro",
+		"Hobbie": "Ver peliculas",
+		"Comida": "Pizza y Encebollado",
+		"Metas": "Graduarme, ir a estudiar en una universidad del extranjero y quedar a vivir ahí",
+	},
+	{
+		"Usuario": "Samantha_Farez",
+		"ColorC": "#ddffb0",
+		"ColorB": "Black",
+		"Foto": "",
+
+		"Nombres": "FAREZ SALINAS GENESIS SAMANTHA",
+		"FechaNacimiento": "09-11-2005",
+		"Signo": "Cancer",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "Bob Marley, Dread Mar I y Willy Rodriguez",
+		"Color": "Verde",
+		"Hobbie": "Caminar",
+		"Comida": "Volquetero",
+		"Metas": "Graduarme , poder ayudar a la comunidad con mi profesión, sustentar mis gastos, ayudar a mi familia y poder viajar por el mundo",
+	},
+	{
+		"Usuario": "Kennedy_Freire",
+		"ColorC": "#ff4747",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "FREIRE ROMERO KENNEDY FABRICIO",
+		"FechaNacimiento": "25-03-2005",
+		"Signo": "Aries",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Adele",
+		"Color": "Morado",
+		"Hobbie": "Escuchar Historias",
+		"Comida": "Carne",
+		"Metas": "Ir a la universidad y conseguir un empleo",
+	},
+	{
+		"Usuario": "Geremy_Heredia",
+		"ColorC": "#ddffb0",
+		"ColorB": "Black",
+		"Foto": "",
+
+		"Nombres": "HEREDIA PROAÑO GEREMY ALBAID",
+		"FechaNacimiento": "06-05-2005",
+		"Signo": "Tauro",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Eminem",
+		"Color": "Verde Fosforecente",
+		"Hobbie": "Jugar Fútbol",
+		"Comida": "Encebollado",
+		"Metas": "Graduarme, estudiar en EE.UU por mi panita",
+	},
+	{
+		"Usuario": "Melani_Llamoca",
+		"ColorC": "#333c87",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "LLAMOCA BRIONES MELANI BLANSHETH",
+		"FechaNacimiento": "03-05-2005",
+		"Signo": "Tauro",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "Kevin Kaarl",
+		"Color": "Azul Cobalto Oscuro",
+		"Hobbie": "Leer",
+		"Comida": "Camarones al ajillo",
+		"Metas": "Estudiar mi carrera universitaria en España, independizarme y  tener un gato persa",
+	},
+	{
+		"Usuario": "Elias_Molina",
+		"ColorC": "Black",
+		"ColorB": "white",
+		"Foto": "",
+
+		"Nombres": "MOLINA MOREIRA ELIAS MIGUEL",
+		"FechaNacimiento": "23-11-2005",
+		"Signo": "Sagitario",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Chano y The Oral Cigarettes",
+		"Color": "Negro",
+		"Hobbie": "Tocar Guitarra",
+		"Comida": "Encebollado",
+		"Metas": "Conseguir una beca para poder Estudiar en el extranjero en Europa o Asia, Graduarme de Economía, o Medicina, Viajar y Hacer música, Lograr tener tiempo con Mi familia y las personas que amo y ser feliz",
+	},
+	{
+		"Usuario": "Mateo_Morales",
+		"ColorC": "#87CEEB",
+		"ColorB": "Black",
+		"Foto": "",
+
+		"Nombres": "MORALES CONZA MATEO JAHIR",
+		"FechaNacimiento": "09-12-2050",
+		"Signo": "Sagitario",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Bad Bunny",
+		"Color": "Azul",
+		"Hobbie": "Jugar Fútbol",
+		"Comida": "Salchipapas",
+		"Metas": "Ser futbolista profesional",
+	},
+	{
+		"Usuario": "Melissa_Orbe",
+		"ColorC": "#7b68fc",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "ORBE VARGAS MELISSA IBET",
+		"FechaNacimiento": "22-04-2005",
+		"Signo": "Tauro",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "BTS",
+		"Color": "Morado",
+		"Hobbie": "Cocinar",
+		"Comida": "Maito",
+		"Metas": "Graduarme de la universidad y conseguir un trabajo estable.",
+	},
+	{
+		"Usuario": "Fernando_Paredes",
+		"ColorC": "#ff4747",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "PAREDES PAUCAR FERNANDO JOSE",
+		"FechaNacimiento": "04-03-2006",
+		"Signo": "Piscis",
+		"Edad": "15",
+		"Sexo": "Masculino",
+		"Artista": "Nicky Jam",
+		"Color": "Rojo",
+		"Hobbie": "Fútbol",
+		"Comida": "Chaulafan",
+		"Metas": "Graduarme y convertirme en policia",
+	},
+	{
+		"Usuario": "Gabriela_Quispe",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "QUISPE TAGUA GABRIELA ELIZABETH",
+		"FechaNacimiento": "31-01-2005",
+		"Signo": "Acuario",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "Karol G",
+		"Color": "Negro",
+		"Hobbie": "Ver doramas",
+		"Comida": "Arroz Marinero",
+		"Metas": "Terminar el colegio para ingresar a la universidad y tener una vida tranquila",
+	},
+	{
+		"Usuario": "Travis_Reatigui",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "",
+
+		"Nombres": "REATIGUI CALLE TRAVIS MATEO",
+		"FechaNacimiento": "26-06-2005",
+		"Signo": "Geminis",
+		"Edad": "15",
+		"Sexo": "Masculino",
+		"Artista": "Will Paquin",
+		"Color": "Negro",
+		"Hobbie": "Basket",
+		"Comida": "Camaron Apanado",
+		"Metas": "Terminar el colegio para ingresar a la universidad y entrar a trabajar para tener una vida tranquila",
+	},
+	{
+		"Usuario": "Slendy_Rodriguez",
+		"ColorC": "#c75af2",
+		"ColorB": "white",
+		"Foto": "",
+
+		"Nombres": "RODRIGUEZ VILEMA SLENDY LISETH",
+		"FechaNacimiento": "",
+		"Signo": "29-05-2005",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "Reik",
+		"Color": "Lila",
+		"Hobbie": "Escuchar música",
+		"Comida": "Encebollado",
+		"Metas": "Graduarme, estudiar la universidad en otro país, ser una gran profesional, apoyar a mi familia en todo ámbito y conocer diversos países.",
+	},
+	{
+		"Usuario": "Aaron_Rugel",
+		"ColorC": "#87CEEB",
+		"ColorB": "black",
+		"Foto": "",
+
+		"Nombres": "RUGEL LEIVA AARON ALEXANDER",
+		"FechaNacimiento": "13-06-2006",
+		"Signo": "Capricornio",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Two Feet",
+		"Color": "Celeste",
+		"Hobbie": "Leer",
+		"Comida": "Arroz Marinero",
+		"Metas": "Graduarme, tener un trabajo estable para en futuro viajar por diferentes países y en la vejes emprender en un negocio",
+	},
+	{
+		"Usuario": "Paola_Solis",
+		"ColorC": "white",
+		"ColorB": "Black",
+		"Foto": "",
+
+		"Nombres": "SOLIS TOASA PAOLA BELEN",
+		"FechaNacimiento": "01-03-2005",
+		"Signo": "Piscis",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "LP y Michael Jackson",
+		"Color": "Blanco",
+		"Hobbie": "Leer",
+		"Comida": "Spaghetti a la boloñesa",
+		"Metas": "Ser aceptada en la armada de la Marina, graduarme e independizarme",
+	},
+	{
+		"Usuario": "",
+		"ColorC": "",
+		"ColorB": "Black",
+		"Foto": "",
+
+		"Nombres": "",
+		"FechaNacimiento": "",
+		"Signo": "",
+		"Edad": "",
+		"Sexo": "",
+		"Artista": "",
+		"Color": "",
+		"Hobbie": "",
+		"Comida": "",
+		"Metas": "",
+	},
+	{
+		"Usuario": "",
+		"ColorC": "",
+		"ColorB": "Black",
+		"Foto": "",
+
+		"Nombres": "",
+		"FechaNacimiento": "",
+		"Signo": "",
+		"Edad": "",
+		"Sexo": "",
+		"Artista": "",
+		"Color": "",
+		"Hobbie": "",
+		"Comida": "",
+		"Metas": "",
+	},
 ]
 
 // document.addEventListener("DOMContentLoaded", lista(estudiantes))
 document.addEventListener("DOMContentLoaded", galeria())
+document.addEventListener("DOMContentLoaded", listar())
 
 function galeria(){
 	for(i of estudiantes){
 		var informacion=`
-		<div class="for">
+		<div class="for" id="${i.Usuario}">
 		<style>
-		.for{
+		#${i.Usuario}{
 			background-color: ${i.ColorC};
+			color: ${i.ColorB};
 		}
 		</style>
-			<img src="${i.Foto}" width="80px"></br>
+			<img src="${i.Foto}" width="80px" align="center"></br>
 			<h2>${i.Nombres}</h2>
 			<p><strong>Fecha de Nacimiento: </strong>${i.FechaNacimiento}</p>
 			<p><strong>Signo Zodiacal: </strong>${i.Signo}</p>
@@ -44,6 +370,13 @@ function galeria(){
 			`
 		document.write(informacion)
 		console.log(i)
+	}
+}
+function listar(){
+	x=1
+	for(i of estudiantes){
+		document.write(`<a href="${i.Usuario}">N:·${x} ${i.Nombres}</a></br>`)
+		x++
 	}
 }
 /*
