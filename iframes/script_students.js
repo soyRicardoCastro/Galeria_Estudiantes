180c9f40b31f4314ab08b461e34fcbe2a8065e2d
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2022-03-02 20:13:18 -0500

    -> Se termina la Base y se dedica solo al diseño

--- iframes/script_students.js
+++ iframes/script_students.js
@@ -17,10 +17,27 @@ var estudiantes= [
 		"Metas": "Estudiar ingeniería en informática, intentar ser uno de los mejores para vivir de lo que me gusta hacer que es la informática",
 	},
 	{
+		"Usuario": "Nicole_Brito",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "https://scontent.fptz1-1.fna.fbcdn.net/v/t1.6435-9/104163228_262590318146074_4204394991466447144_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeE9svw2qxC6fb5WuSt1_JhDOYKh1YMHfWk5gqHVgwd9aQBlKGomq5I50tss8Q-h_wzDBNqLNovnCLk_dC2uAXVr&_nc_ohc=0waeLPedgNYAX-xOztD&_nc_ht=scontent.fptz1-1.fna&oh=00_AT9dcMt-Wc6ZSfkvkuURWdtfhuF7wcgLSXsgThhXYbYqkQ&oe=62463507",
+
+		"Nombres": "BRITO SERRANO ALLISSON NICOLE",
+		"FechaNacimiento": "31-01-2005",
+		"Signo": "Acuario",
+		"Edad": "17",
+		"Sexo": "Femenino",
+		"Artista": "Daddy Yanke",
+		"Color": "Blanco",
+		"Hobbie": "Jugar videojuegos",
+		"Comida": "Hamburgesas y Papas",
+		"Metas": "Pasar de año y graduarme",
+	},
+	{
 		"Usuario": "Joyce_Canelos",
 		"ColorC": "Black",
 		"ColorB": "White",
-		"Foto": "",
+		"Foto": "https://scontent.fptz1-1.fna.fbcdn.net/v/t39.30808-6/239345686_4009493462493119_1307849700224307628_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHq_G-JNRFQwolpK8_XA7Pod6M1X2v26wN3ozVfa_brA2zeECL4VMuCIjzEnKao3AuGFkvjSMitdw71JFBiC52z&_nc_ohc=gTjQN8O7ERsAX_GY80Y&_nc_ht=scontent.fptz1-1.fna&oh=00_AT8hPZn393qdPnXBRDQtegN1vxNfHzfKlDVZZke8DALr4A&oe=622435A1",
 
 		"Nombres": "CANELOS ARANDA JOYCE GARDENIA",
 		"FechaNacimiento": "15-10-2004",
@@ -37,7 +54,7 @@ var estudiantes= [
 		"Usuario": "Ramiro_Chavez",
 		"ColorC": "#3863ff",
 		"ColorB": "White",
-		"Foto": "",
+		"Foto": "https://scontent.fuio3-1.fna.fbcdn.net/v/t1.15752-9/274808733_333841345461950_3755319158803803789_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeHZH-tPijCfwRrNxr_MP4LwZHS2Lt9nZkhkdLYu32dmSCOK7YVj9tOT6FnMjjIIvadjmYcEQL7-odEPbulPT3gH&_nc_ohc=KlOyO4OvtsMAX9FVdLl&_nc_ht=scontent.fuio3-1.fna&oh=03_AVL2DKd1zMQCOXOVzO2Vi2vMhWmiJXgeqszkRJMEtwfwdw&oe=622149BB",
 
 		"Nombres": "CHAVEZ MEJIA RAMIRO ISRAEL",
 		"FechaNacimiento": " 04-10-2005",
@@ -66,6 +83,23 @@ var estudiantes= [
 		"Hobbie": "Ciclismo",
 		"Comida": "Camarones Apanados",
 		"Metas": "Graduarme y ser una doctora psicacultural para acompañarle Ami hermano en los tours y conocer todos los lugares de este mundo antes de morir",
+	},
+	{
+		"Usuario": "Elkin_Estrada",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "https://scontent.fptz1-1.fna.fbcdn.net/v/t1.15752-9/274681002_635005784463664_948825919273377374_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEfiyFHJfmyQdkf1UZEpvWFK7XFyLVACaErtcXItUAJoTLFWLahIUe2X4wVrkLqSNDAdoiBHS0uBasBoQdn-YkG&_nc_ohc=XLxdEaDVxDwAX_bMGdI&_nc_ht=scontent.fptz1-1.fna&oh=03_AVKo9-MrtQM7cHwdqFxvYS0vfjMI_Bp5PmVNXyJIARvXSA&oe=62463965",
+
+		"Nombres": "ESTRADA AGUIRRE ELKIN JAVIER",
+		"FechaNacimiento": "05-01-2006",
+		"Signo": "Capricornio",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Jhay Cortez",
+		"Color": "Negro",
+		"Hobbie": "Videojuegos",
+		"Comida": "Emborrajado",
+		"Metas": "Tener un apartamento en la ciudad, saber cómo llevar mi dinero y hacer lo que me gusta",
 	},
 	{
 		"Usuario": "Erick_Estrella",
@@ -105,7 +139,7 @@ var estudiantes= [
 		"Usuario": "Kennedy_Freire",
 		"ColorC": "#ff4747",
 		"ColorB": "White",
-		"Foto": "",
+		"Foto": "https://cdn.fbsbx.com/v/t59.2708-21/271848051_204820268511017_3727019031714049396_n.gif?_nc_cat=111&ccb=1-5&_nc_sid=041f46&_nc_eui2=AeFf0vHgzhs8VEzAjTzOCr8oP_UlOxMbyMQ_9SU7ExvIxK1TLeyvVlE3y7t7eY42L_7GySD6Q6e1lnTEqP_pTUTe&_nc_ohc=DNf1FKFH_rEAX92YAJV&_nc_ht=cdn.fbsbx.com&oh=03_AVIu_MhSTNaQK1qdhlI_KRUvZ_J2TPZYPNLzmU0B7Mpj7A&oe=6221C72C",
 
 		"Nombres": "FREIRE ROMERO KENNEDY FABRICIO",
 		"FechaNacimiento": "25-03-2005",
@@ -119,10 +153,27 @@ var estudiantes= [
 		"Metas": "Ir a la universidad y conseguir un empleo",
 	},
 	{
+		"Usuario": "Anahi_Gonzales",
+		"ColorC": "Black",
+		"ColorB": "White",
+		"Foto": "https://scontent.fptz1-1.fna.fbcdn.net/v/t1.15752-9/274100689_3107000032892402_8587000360075854003_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeETLZ5OxPHNn5xhBVbdWwo1wLvDiri3ZEnAu8OKuLdkSfFylmO888r8vPw6BwI9piLzK1ZEM8tzu3M16e44BwsV&_nc_ohc=XJ16mNuUqVgAX9f5Ulk&_nc_ht=scontent.fptz1-1.fna&oh=03_AVKmxBhFfZEO468A8JYmRDulpFCIJvA7TVO56S0v7E9bGw&oe=62456E6E",
+
+		"Nombres": "GONZALEZ CAMBAL ANAHI FERNANDA",
+		"FechaNacimiento": "14-05-2005",
+		"Signo": "Tauro",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "Bad Bunny",
+		"Color": "Negro",
+		"Hobbie": "Jugar videojuegos o jugar Básquet ",
+		"Comida": "Maito o comida rápida ",
+		"Metas": "Graduarme del colegio",
+	},
+	{
 		"Usuario": "Geremy_Heredia",
 		"ColorC": "#ddffb0",
 		"ColorB": "Black",
-		"Foto": "",
+		"Foto": "https://scontent.fuio3-1.fna.fbcdn.net/v/t1.15752-9/274350061_1116446558897710_2958774408537129918_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeEDNuj7w7tSo8dh_NpsUK9I990Q6eC8uuH33RDp4Ly64eyAVbIG3WGJSMb6A-wfhstx_K2sNMa7JczeYLh63qB0&_nc_ohc=yJ16V9-a2GcAX9RdY4j&tn=lzhpH_UQoiHVEhp5&_nc_ht=scontent.fuio3-1.fna&oh=03_AVKC2g0g9fhoaiPoOMGpLKzXeM8CKD4Ry7hRqadovy_RAA&oe=62211DF7",
 
 		"Nombres": "HEREDIA PROAÑO GEREMY ALBAID",
 		"FechaNacimiento": "06-05-2005",
@@ -139,7 +190,7 @@ var estudiantes= [
 		"Usuario": "Melani_Llamoca",
 		"ColorC": "#333c87",
 		"ColorB": "White",
-		"Foto": "",
+		"Foto": "https://scontent.fuio3-1.fna.fbcdn.net/v/t1.15752-9/274213995_537056367605355_7089527700398862833_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeGnKbxUgmhX94dtsErDNp1on9zaVp9cx5qf3NpWn1zHmrGLqb6_NIw36aEcyWFTD4lBNrNotkx2FkMRNOgIsdTL&_nc_ohc=D-0mKQOyYzkAX_Gcz1S&_nc_ht=scontent.fuio3-1.fna&oh=03_AVKQc_vaXO2h4AbpxeK_kGfLhYsxoNWrWPobSbq6t9Df5w&oe=62460D06",
 
 		"Nombres": "LLAMOCA BRIONES MELANI BLANSHETH",
 		"FechaNacimiento": "03-05-2005",
@@ -156,7 +207,7 @@ var estudiantes= [
 		"Usuario": "Elias_Molina",
 		"ColorC": "Black",
 		"ColorB": "white",
-		"Foto": "",
+		"Foto": "https://scontent.fptz1-1.fna.fbcdn.net/v/t1.15752-9/274548015_504962047869559_3263842478592146482_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeGoIJ0uRuK3KXqH0_90sk-CDRpK17RdRdoNGkrXtF1F2urzxM_U_fURnz_RfQuQenaIMsFGp80Vf6jZTLKC2aWs&_nc_ohc=JB8_86yuoa0AX8dQfss&_nc_ht=scontent.fptz1-1.fna&oh=03_AVLXEWnGUkhT7kNW_SwhndKH5Bh65OODwEQtwfzpKSxa8Q&oe=6244F132",
 
 		"Nombres": "MOLINA MOREIRA ELIAS MIGUEL",
 		"FechaNacimiento": "23-11-2005",
@@ -173,7 +224,7 @@ var estudiantes= [
 		"Usuario": "Mateo_Morales",
 		"ColorC": "#87CEEB",
 		"ColorB": "Black",
-		"Foto": "",
+		"Foto": "https://scontent.fuio3-1.fna.fbcdn.net/v/t1.15752-9/274822512_1336694123482057_7614695623911648975_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeFuW2LKSuZ3EHl8rUkOktk7L-tbAUT65UMv61sBRPrlQ7l7k_QVfwHFSnWgJP4YER7YX_HuBwMzwEV60adXp5iy&_nc_ohc=31-gyyxgzvIAX_E9MJL&_nc_oc=AQlZElePjUkAnXyCBViWTM22O3yitU4SVjpQi008isBf_HHk_XDH4DfNjo1P0vzcVMmiys1NHhUczhItkD4AlAqX&_nc_ht=scontent.fuio3-1.fna&oh=03_AVKMwTJD391gH-tyPIKsyzhdZxG-gnEi3JSIPOCVeCOfQA&oe=6220FE13",
 
 		"Nombres": "MORALES CONZA MATEO JAHIR",
 		"FechaNacimiento": "09-12-2050",
@@ -207,7 +258,7 @@ var estudiantes= [
 		"Usuario": "Fernando_Paredes",
 		"ColorC": "#ff4747",
 		"ColorB": "White",
-		"Foto": "",
+		"Foto": "https://scontent.fuio3-1.fna.fbcdn.net/v/t1.15752-9/273823966_698529497813568_2062885152694579741_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeGLxaVQliyVeZ3wWDNGd_UU-ctP5c_EqXX5y0_lz8Spda5c6f8MSpO6d1EfeX7d70tkWUh3VJYlVy0aP51YHmX2&_nc_ohc=ZrUFCEgb5gAAX-9RyWe&_nc_ht=scontent.fuio3-1.fna&oh=03_AVJs0xKaM9yctxnPOvYRKb_wOqvhkYSxeaZ4WZB6s_pAhw&oe=6220EFAA",
 
 		"Nombres": "PAREDES PAUCAR FERNANDO JOSE",
 		"FechaNacimiento": "04-03-2006",
@@ -224,7 +275,7 @@ var estudiantes= [
 		"Usuario": "Gabriela_Quispe",
 		"ColorC": "Black",
 		"ColorB": "White",
-		"Foto": "",
+		"Foto": "https://scontent.fuio3-1.fna.fbcdn.net/v/t1.15752-9/274556782_1147778765996698_4037527112463143764_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeGl5nSjZxu_Kg7OqgVL9Vv3p5mdBk5ozsCnmZ0GTmjOwKy6C45udGjLzSyhI7KYxiQvzxyiFJ0miN39xSOiRsqP&_nc_ohc=cxuDHq50L3wAX_Ry5vN&tn=lzhpH_UQoiHVEhp5&_nc_ht=scontent.fuio3-1.fna&oh=03_AVIkzVViBsZYWGVhKpoc8_7qyY-gg_ZzwhoI3Y1aMTzG9A&oe=62211CE0",
 
 		"Nombres": "QUISPE TAGUA GABRIELA ELIZABETH",
 		"FechaNacimiento": "31-01-2005",
@@ -275,7 +326,7 @@ var estudiantes= [
 		"Usuario": "Aaron_Rugel",
 		"ColorC": "#87CEEB",
 		"ColorB": "black",
-		"Foto": "",
+		"Foto": "https://cdn.fbsbx.com/v/t59.2708-21/275042314_1471633316564217_8014084350730971746_n.gif?_nc_cat=107&ccb=1-5&_nc_sid=041f46&_nc_eui2=AeEclbO0FjqkDpqY8Hp27zTSg-TTWYpXjM-D5NNZileMz0c7ueNmNsbZy0eazLcKLbd9idtR7Lcrz4LIKm_dAEpD&_nc_ohc=FNjZTiy8efwAX_konrn&_nc_ht=cdn.fbsbx.com&oh=03_AVJIHhDHBk6coPK3cEZoYSRrY7EjJ5BkAZdVFkQQTZh0cQ&oe=62212BED",
 
 		"Nombres": "RUGEL LEIVA AARON ALEXANDER",
 		"FechaNacimiento": "13-06-2006",
@@ -292,7 +343,7 @@ var estudiantes= [
 		"Usuario": "Paola_Solis",
 		"ColorC": "white",
 		"ColorB": "Black",
-		"Foto": "",
+		"Foto": "https://scontent.fptz1-1.fna.fbcdn.net/v/t1.15752-9/274701577_1094829114413047_5104168810372224368_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeF-a3bZdkTKLjNoyFyHA3sboXmER_7cKQ6heYRH_twpDmhPXUzm2puF6YLzfT-NGb3Pkx1X7k-0hzUTWANilhVU&_nc_ohc=_yrhAYMfLlIAX98nHSG&_nc_ht=scontent.fptz1-1.fna&oh=03_AVJa_YtmAm91norbVdORjfB1U1KIHLc-4dubdxCXafcQHQ&oe=6244A31F",
 
 		"Nombres": "SOLIS TOASA PAOLA BELEN",
 		"FechaNacimiento": "01-03-2005",
@@ -306,57 +357,58 @@ var estudiantes= [
 		"Metas": "Ser aceptada en la armada de la Marina, graduarme e independizarme",
 	},
 	{
-		"Usuario": "",
-		"ColorC": "",
-		"ColorB": "Black",
+		"Usuario": "Ninette_Tandazo",
+		"ColorC": "#7b68fc",
+		"ColorB": "White",
 		"Foto": "",
 
-		"Nombres": "",
-		"FechaNacimiento": "",
-		"Signo": "",
-		"Edad": "",
-		"Sexo": "",
-		"Artista": "",
-		"Color": "",
-		"Hobbie": "",
-		"Comida": "",
-		"Metas": "",
+		"Nombres": "TANDAZO GARCIA NINETTE JORIETH",
+		"FechaNacimiento": "16-10-2005",
+		"Signo": "Libra",
+		"Edad": "16",
+		"Sexo": "Femenino",
+		"Artista": "BTS",
+		"Color": "Morado",
+		"Hobbie": "Ver Videos",
+		"Comida": "Guatita",
+		"Metas": "Poder mantenerme a mi misma cuando me independice",
 	},
 	{
-		"Usuario": "",
-		"ColorC": "",
+		"Usuario": "Anthony_Zurita",
+		"ColorC": "#ffcf4a",
 		"ColorB": "Black",
 		"Foto": "",
 
-		"Nombres": "",
-		"FechaNacimiento": "",
-		"Signo": "",
-		"Edad": "",
-		"Sexo": "",
-		"Artista": "",
-		"Color": "",
-		"Hobbie": "",
-		"Comida": "",
-		"Metas": "",
+		"Nombres": "ZURITA GAIBOR ANTHONY JOEL",
+		"FechaNacimiento": "05-09-2005",
+		"Signo": "Virgo",
+		"Edad": "16",
+		"Sexo": "Masculino",
+		"Artista": "Alzate",
+		"Color": "Amarillo",
+		"Hobbie": "Correr en Motos",
+		"Comida": "Camarones al Ajillo",
+		"Metas": "Ser Oficial de la Policía, y conocer muchos países.",
 	},
 ]
 
 // document.addEventListener("DOMContentLoaded", lista(estudiantes))
-document.addEventListener("DOMContentLoaded", galeria())
 document.addEventListener("DOMContentLoaded", listar())
+document.addEventListener("DOMContentLoaded", galeria())
 
 function galeria(){
 	for(i of estudiantes){
 		var informacion=`
 		<div class="for" id="${i.Usuario}">
-		<style>
-		#${i.Usuario}{
-			background-color: ${i.ColorC};
-			color: ${i.ColorB};
-		}
-		</style>
-			<img src="${i.Foto}" width="80px" align="center"></br>
+			<style>
+				#${i.Usuario}{
+				background-color: ${i.ColorC};
+				color: ${i.ColorB};
+			}
+			</style>
+			
 			<h2>${i.Nombres}</h2>
+			<img src="${i.Foto}" height="190px" align="center"></br>
 			<p><strong>Fecha de Nacimiento: </strong>${i.FechaNacimiento}</p>
 			<p><strong>Signo Zodiacal: </strong>${i.Signo}</p>
 			<p><strong>Edad:</strong> ${i.Edad} años</p>
@@ -375,7 +427,11 @@ function galeria(){
 function listar(){
 	x=1
 	for(i of estudiantes){
-		document.write(`<a href="${i.Usuario}">N:·${x} ${i.Nombres}</a></br>`)
+		document.write(`
+		<div class="list">
+			<a href="#${i.Usuario}">N:·${x} ${i.Nombres}</a></br>
+		</div>
+			`)
 		x++
 	}
 }
